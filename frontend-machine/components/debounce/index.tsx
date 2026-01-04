5572a3207ce3d158778c7afc8e1636a51299a110
Kofuku0 <khushilalit369@gmail.com>
2026-01-04 10:46:48 +0530

    refactor: fixed th formatting

--- frontend-machine/components/debounce/index.tsx
+++ frontend-machine/components/debounce/index.tsx
@@ -1,56 +1,53 @@
-'use client';
-import React, { useEffect }  from 'react'
-import useDebounce from '@/frontend-machine/hooks/useDebounce';
-interface ProductProps{
-     id:number;
-     title:string
+"use client";
+import React, { useEffect } from "react";
+import useDebounce from "@/frontend-machine/hooks/useDebounce";
+interface ProductProps {
+  id: number;
+  title: string;
 }
 
 const index = () => {
-    const [searchTerm,setSearchTerm] = React.useState("");
-    const [data,setData] =  React.useState<ProductProps[]|null>([]);
-    const handleChange = (e:any)=>{
-        setSearchTerm(e.target.value);
-    }
- 
-    const debouncedSearchTerm = useDebounce({searchTerm,duration:500});
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [data, setData] = React.useState<ProductProps[] | null>([]);
+  const handleChange = (e: any) => {
+    setSearchTerm(e.target.value);
+  };
 
-    useEffect(()=>{
-        if(debouncedSearchTerm.trim()==='') return;
+  const debouncedSearchTerm = useDebounce({ searchTerm, duration: 500 });
 
-        const fetchData= async ()=>{
-           const res = await fetch(`https://dummyjson.com/products/search?q=${debouncedSearchTerm}&limit=10`);
-           const ans = await res.json();
-           console.log(ans,"ans")
-           setData(ans.products);
-        }
-        fetchData()
+  useEffect(() => {
+    if (debouncedSearchTerm.trim() === "") return;
 
-    },[debouncedSearchTerm])
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://dummyjson.com/products/search?q=${debouncedSearchTerm}&limit=10`
+      );
+      const ans = await res.json();
+      console.log(ans, "ans");
+      setData(ans.products);
+    };
+    fetchData();
+  }, [debouncedSearchTerm]);
 
   return (
-    <div className='w-full flex flex-col items-center justify-center '>
-   
-
+    <div className="w-full flex flex-col items-center justify-center ">
       <input
-      type="text"
-      placeholder="enter the text"
-      onChange = {handleChange}
-      className='border border-gray-300 rounded-md p-2 mt-5 w-100'/>
+        type="text"
+        placeholder="enter the text"
+        onChange={handleChange}
+        className="border border-gray-300 rounded-md p-2 mt-5 w-100"
+      />
 
-     <div className='p-2'>Serach {searchTerm} Results</div>
-     <div>
-         {data?.map((i,ix)=>{
-             return <div key={ix}>
-                  {i.title}
-                </div>
-         })}
-     </div>
+      <div className="p-2">Serach {debouncedSearchTerm} Results</div>
+      <div>
+        {data?.map((i, ix) => {
+          return <div key={ix}>{i.title}</div>;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default index
-
+export default index;
 
 //  items= await  fetch(`https://dummyjson.com/products/search?q=${debouncedSearchTerm}`);
--- frontend-machine/hooks/useDebounce.ts
+++ frontend-machine/hooks/useDebounce.ts
@@ -1,23 +1,17 @@
+import { useEffect, useState, useRef } from "react";
 
+export default function useDebounce({ searchTerm, duration }: any) {
+  const [debouncedTerm, setDebouncedTerm] = useState(searchTerm);
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-import { useEffect, useState ,useRef } from "react"
+  useEffect(() => {
+    if (timerRef.current != null) clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => setDebouncedTerm(searchTerm), duration);
 
-export default function useDebounce({searchTerm,duration}:any){
-      const [debouncedTerm,setDebouncedTerm] = useState(searchTerm);
-       const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
-      
-      useEffect(()=>{
-        if(timerRef.current !=null) clearTimeout(timerRef.current);
-        timerRef.current = setTimeout(()=>setDebouncedTerm(searchTerm),duration);
+    return () => {
+      if (timerRef.current != null) clearTimeout(timerRef.current);
+    };
+  }, [searchTerm, duration]);
 
-
-        return ()=>{
-             if(timerRef.current !=null) clearTimeout(timerRef.current);
-        }
-      },[searchTerm,duration])
-
-
-
-      return debouncedTerm;
-
+  return debouncedTerm;
 }
