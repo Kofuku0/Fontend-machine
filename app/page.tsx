aab1cf5ed961759b5a388683b5e884d2766961de
Kofuku0 <khushilalit369@gmail.com>
2026-01-25 19:04:58 +0530

    feat: added the accordian ii

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,11 @@ import React from "react";
 // import OTP from "@/frontend-machine/components/otp";
 // import Accordion from "@/frontend-machine/components/accordian";
 // import Tabs from "@/frontend-machine/components/tabs";
-import DataTable from "@/frontend-machine/components/data-table";
+// import DataTable from "@/frontend-machine/components/data-table";
+import AccordianII from "@/frontend-machine/components/accordian-ii";
 export default function HomePage() {
 //   const [open,setOpen] = React.useState(false);
+
   return (
      <div>
       {/* <InfiniteScroll /> */}
@@ -54,8 +56,23 @@ export default function HomePage() {
           },
         ]}
       /> */}
-      <DataTable/>
- 
+      {/* <DataTable/> */}
+    <AccordianII 
+       sections = {[{
+        title: 'HTML',
+        value :'html',
+        content: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.'
+       },{
+        title: 'CSS',
+        value :'css',
+        content:  'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.'
+       },{
+        title: 'JavaScript',
+        value :'javascript',
+        content: 'JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.'
+       }]}
+    />
+       
      </div>
   );
 }
---
+++ frontend-machine/components/accordian-ii/index.tsx
@@ -0,0 +1,45 @@
+import { collectMeta } from "next/dist/build/utils";
+import React, { useState } from "react";
+
+interface sectionProps {
+  title: string;
+  value: string;
+  content: string;
+}
+
+interface sectionsProps {
+  sections: sectionProps[];
+}
+export default function AccordianII({ sections }: sectionsProps) {
+  const [openSection, setOpenSection] = useState(new Set<string>());
+  return (
+    <div>
+      {sections.map(({ title, value, content }) => {
+        const isExanded = openSection.has(value);
+        return (
+          <div key={value} className="my-2 mx-2 ">
+
+            <div className="w-full px-5 py-6 flex items-center justify-between rounded-xl bg-gray-500">
+              <div className="">{title}</div>
+              <div
+                onClick={() => {
+                   const newSet = new Set(openSection);
+                   if(!newSet.has(value)){
+                    newSet.add(value);
+                   }else{
+                      newSet.delete(value);
+                   }
+                   setOpenSection(newSet);
+                }}
+                className="mr-2"
+              >
+                {isExanded ? "−" : "+" }
+              </div>
+            </div>
+            <div hidden={!isExanded}>{content}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
